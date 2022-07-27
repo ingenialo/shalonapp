e962bf12b62f1495834674806db6d85d0b4e210b
jorgeeliecerpalacios <jorge.palacios652@gmailcom>
2022-07-27 17:20:22 -0500

    create table to payments and clients

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
+class Client(models.Model):
+    
+    id = models.AutoField(auto_created=True)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.EmailField(unique=True)
+    identification_number = models.CharField(blank=False, unique=True) 
+    phone = models.CharField(unique=True, blank=False)
+    second_phone = models.CharField(unique=True, blank=True)
+    age = models.IntegerField(blank=True)
+    birth_day = models.IntegerField(blank=True)
+    birth_month = models.IntegerField(blank=True)
+    birth_year = models.IntegerField(blank=True)
+    record_number = models.IntegerField(blank=True)
+    address = models.CharField(blank=False)
+    district = models.CharField(blank=True)
+    city = models.CharField(blank=True)
+    
+
 # Create your models here.
--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+# from clients.models import Client
+
+
+class Payments(models.Model):
+
+    id = models.AutoField(auto_created=True)
+    payment_date = models.DateTimeField(auto_now_add=True)
+    location_id = models.IntegerField()
+    location_name = models.IntegerField()
+    # amount = models.IntegerFieldField(required=True)
+    # paid_amount = models.IntegerField()
+    # change_amount = models.IntegerField()
+    employee_code_id = models.IntegerField()
+    employee_name = models.CharField()
+    # client = [Client]
+    
+    
 # Create your models here.
