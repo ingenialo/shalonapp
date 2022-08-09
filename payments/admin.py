486ca459ac1a1dfc16582c4fbded8b700c4f6302
jorgeeliecerpalacios <jorge.palacios652@gmailcom>
2022-08-09 11:36:47 -0500

    create admin and migrate the amount in payments

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
 # Register your models here.
+from payments.models import Payment
+
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+
+
+# Register your models here.
+    list_display = (
+        'pk', 
+        'payment_date', 
+        'location_id', 
+        'location_name', 
+        'amount', 
+        'paid_amount',
+        'change_amount',
+        'employee_code_id',
+        'employee_name',
+        'client'
+        )
+    # list_display_links = ('pk')
+    # list_editable = ('phone_number', 'website', 'picture')
+    search_fields = []        
+    list_filter = []
+    
+    
+    
+    
+    
+    
+    # 
+     
+    
+    
+    
+    
--- payments/models.py
+++ payments/models.py
@@ -1,15 +1,16 @@
+from numbers import Number
 from django.db import models
 
 from clients.models import Clients
 
 
-class Payments(models.Model):
+class Payment(models.Model):
 
     # id = models.AutoField(auto_created=True)
     payment_date = models.DateTimeField(auto_now_add=True)
     location_id = models.IntegerField(null=True)
     location_name = models.IntegerField(null=True)
-    # amount = models.IntegerFieldField(required=True)
+    amount = models.IntegerField()
     paid_amount = models.IntegerField(null=True)
     change_amount = models.IntegerField(null=True)
     employee_code_id = models.IntegerField(null=True , blank=True)
