2116ce96cc3272d70d4ae27b0ba459538545fc94
Andres Cuervo <andresmauro17@gmail.com>
2022-08-20 11:55:45 -0500

    fix nmodel and refactorize .view

--- apps/clients/models.py
+++ apps/clients/models.py
@@ -3,21 +3,21 @@ from django.db import models
 
 class Clients(models.Model):
     
-    agenda_id = models.IntegerField(null=True, unique=True)
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
-    email = models.EmailField(unique=True)
-    identification_number = models.CharField(max_length=200 ,blank=False, unique=True) 
-    phone = models.CharField(max_length=200, blank=False)
+    agenda_id = models.IntegerField(blank=True,null=True, unique=True)
+    first_name = models.CharField(max_length=200,blank=True,null=True)
+    last_name = models.CharField(max_length=200,blank=True,null=True)
+    email = models.EmailField(unique=True,blank=True,null=True)
+    identification_number = models.CharField(max_length=200, unique=True, blank=True,null=True) 
+    phone = models.CharField(max_length=200, blank=True,null=True)
     second_phone = models.CharField(max_length=200 ,unique=True, blank=True,null=True)
-    age = models.IntegerField(blank=True, null=True,default=None)
-    birth_day = models.IntegerField(blank=True, null=True,default=None)
-    birth_month = models.IntegerField(blank=True, null=True,default=None)
-    birth_year = models.IntegerField(blank=True, null=True,default=None)
-    record_number = models.IntegerField(blank=True, null=True,default=None)
-    address = models.CharField(max_length=200 ,blank=False)
-    district = models.CharField(max_length=200 ,blank=True)
-    city = models.CharField(max_length=200 ,blank=True)
+    age = models.IntegerField(blank=True,null=True)
+    birth_day = models.IntegerField(blank=True,null=True)
+    birth_month = models.IntegerField(blank=True,null=True)
+    birth_year = models.IntegerField(blank=True,null=True)
+    record_number = models.IntegerField(blank=True,null=True)
+    address = models.CharField(max_length=200 ,blank=True,null=True)
+    district = models.CharField(max_length=200, blank=True,null=True)
+    city = models.CharField(max_length=200 ,blank=True,null=True)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
--- apps/payments/views.py
+++ apps/payments/views.py
@@ -5,62 +5,53 @@ import json
 from pprint import pprint
 from apps.clients.models import Clients
 from apps.payments.models import Payment
-# from apps.clients.serializer import ClientSerializer
 
 def conv(value):
     if value:
-        print("retorno el valor")
         return value
     else:
-        print("seria none")
         return None
 
 # Create your views here.
 def list_payments(request):
-    # respuesta = ""
-    
-    # url = "https://api.github.com/users/jorgeeliecerpalacios"
     url = "https://agendapro.com/api/public/v1/payments"
-
-    
     headers = {
     "Accept": "application/json",
     "Authorization": "Basic MWk4c3RicDY6dGd5ZWRqd3FtZjM0ejNwdGttNjM0bTJid2llM28zanBjZWhtbWI0Zg=="
     }
-
     response = requests.get(url, headers=headers) 
-    
+    response_json =response.json()
+    f = open("./apps/payments/salida.json", "w")
+    f.write(json.dumps(response_json))
+    f.close()
     if response.status_code == 200:
-        for paymentjson in response.json()["payments"]:
+        for paymentjson in response_json["payments"]:
             clientejson=paymentjson["client"]
             clientedb = Clients.objects.filter(identification_number=clientejson["identification_number"]).first()
             if(not clientedb):
-                print("no hay cliente")
-                print(clientejson['first_name'])
                 cliente = {
-                            'agenda_id' : clientejson['id'],
-                            'first_name' : clientejson['first_name'],
-                            'last_name' : clientejson['last_name'] ,
-                            'email' : clientejson['email'] ,
-                            'identification_number' : clientejson['identification_number']  ,
-                            'phone' : clientejson['phone'] ,
+                            'agenda_id' : conv(clientejson['id']),
+                            'first_name' : conv(clientejson['first_name']),
+                            'last_name' : conv(clientejson['last_name']),
+                            'email' : conv(clientejson['email']),
+                            'identification_number' : conv(clientejson['identification_number']),
+                            'phone' : conv(clientejson['phone']),
                             'second_phone' : conv(clientejson['second_phone']),
                             'age' : conv(clientejson['age']),
                             'birth_day' : conv(clientejson['birth_day']),
                             'birth_month' : conv(clientejson['birth_month']),
                             'birth_year' : conv(clientejson['birth_year']),
                             'record_number' : conv(clientejson['record_number']),
-                            'address' : clientejson['address'],
-                            'district' : clientejson['district'],
-                            'city' : clientejson['city'],
+                            'address' : conv(clientejson['address']),
+                            'district' : conv(clientejson['district']),
+                            'city' : conv(clientejson['city']),
                         }
-                print(cliente)
                 clientdb = Clients(**cliente)
                 clientdb.save()
-                print(clientdb)
-            
-        
-        # paymentdb = Payment.objects.filter(agenda_id=clientejson["id"]).first()
+    return HttpResponse('<h1> Get data from agenda pro successfully <span>&#128512;</span> </h1>')
+
+
+ # paymentdb = Payment.objects.filter(agenda_id=clientejson["id"]).first()
         
         # if(not paymentdb):
         #     print("no hay cliente")
@@ -82,192 +73,34 @@ def list_payments(request):
         #     paymentdb = Payments(**pago)
         #     paymentdb.save()
         #     print(paymentdb)
-    
-    
-    # cliente = Clients.objects.get(pk=1)
-    # # todoslosclientes = Clients.objects.all()
-    # print(todoslosclientes)
-     
-     
-     
-     
-     
-     
-     
-     
-
-    # if response.status_code == 200:
-        
-    #     paymentjson=response.json()["payments"][0]["client"]
-        
-    #     if (paymentjson):
-            
-    #         # cliente 
-          
-                    
-    #         clientejson = paymentjson["client"]
-    #         clientedb = Clients.objects.filter(agenda_id=clientejson["id"]).first()
-    #         if(not clientedb):
-    #             clientdb = Clients(
-                    
-    #                 client = {
-    #                     'agenda_id' : '',
-    #                     'first_name' : '',
-    #                     'last_name' : '' ,
-    #                     'email' : '' ,
-    #                     'identification_number' : '' ,
-    #                     'phone' : '',
-    #                     'second_phone' : None,
-    #                     'age' : None,
-    #                     'birth_day' : None,
-    #                     'birth_month' : None,
-    #                     'birth_year' : None,
-    #                     'record_number' : '',
-    #                     'address' : '',
-    #                     'district' : '',
-    #                     'city' : '',
-    #                 }
-                    
-    #             )
-    #             clientdb.save()
-    #             print(clientdb)
-            
-    #         # payment
-    #         paymentdb = Payment.objects.filter(agenda_id=clientejson["id"]).first()
-    #         if(not paymentdb):
-    #             paymentdb = Payment(
-               
-    #                 pago = {
-    #                     'agenda_id' : '',
-    #                     'payment_date' : '',
-    #                     'location_id' : '',
-    #                     'location_name' : '',
-    #                     'amount' : None,
-    #                     'paid_amount' : None,
-    #                     'change_amount' : None,
-    #                     'employee_code_id' : None,
-    #                     'employee_name' : '',
-    #                     'client' : clientdb,
-                    
-    #             }
-    #             )
-                
-    #             paymentdb.save()
-    #             print(paymentdb)
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    # for row in response:
-    #     print(row)
-        # clients = Clients(row)
-        # print(clients)
-        # clients.save()
-        # print(clients.first_name)
-    
-    
-    # client = Client(
-    #     first_name = {response[8]},
-    #     # last_name =  {response[1]},
-    #     # email =  {response[1]},
-    #     # identification_number =  {response[1]},
-    #     # phone =  {response[1]},
-    #     # second_phone =  {response[1]},
-    #     # age =  {response[1]},
-    #     # birth_day =  {response[1]},
-    #     # birth_month =  {response[1]},
-    #     # birth_year =  {response[1]},
-    #     # record_number =  {response[1]},
-    #     # address =  {response[1]},
-    #     # district =  {response[1]},
-    #     # city =  {response[1]},
-    #     )
-    # client.save()
-    
-    
-    #     else:
-    # print('Error')
-
-    
-    
-    # # Diccionario
-   
-   
-    
-    
-
-    
-    
-    return HttpResponse('hola mundo')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ 
 
 
 def test(request):
-#     cliente = Clients.objects.get(id=1)
-#     todoslosclientes = Clients.objects.all()
-#     print(todoslosclientes)
-#     pago = {
-#         'agenda_id' : '123344',
-#         'payment_date' : '12345678',
-#         'location_id' : '98765',
-#         'location_name' : 'jpl',
-#         'amount' : None,
-#         'paid_amount' : None,
-#         'change_amount' : None,
-#         'employee_code_id' : None,
-#         'employee_name' : '',
-#         'client' : cliente,
-#         "pija":"megusta",
+    cliente = Clients.objects.get(id=1)
+    todoslosclientes = Clients.objects.all()
+    print(todoslosclientes)
+    pago = {
+        'agenda_id' : '123344',
+        'payment_date' : '12345678',
+        'location_id' : '98765',
+        'location_name' : 'jpl',
+        'amount' : None,
+        'paid_amount' : None,
+        'change_amount' : None,
+        'employee_code_id' : None,
+        'employee_name' : '',
+        'client' : cliente,
        
-#     }
-#     paymentpija = Payment (**pago)
+    }
+    paymentsaved = Payment (**pago)
     
-#     paymentpija.save()
+    paymentsaved.save()
     
-#     pagodos=Payment.objects.filter(id=1)
-#     pagodos=pagodos[0]
-#     print(pagodos)
-#     # if(pagodos):
-#     #     print(pagodos)
-#     #     pagodos.delete()
-    
-    return HttpResponse('mkon')
+    pagodos=Payment.objects.filter(id=1)
+    pagodos=pagodos[0]
+    print(pagodos)
+    if(pagodos):
+        print(pagodos)
+        pagodos.delete()
+    return HttpResponse('testing.....')
