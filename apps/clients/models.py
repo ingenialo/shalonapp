8128779dab3fc21ab70b302a01ee96dfc8a0905c
Andres Cuervo <andresmauro17@gmail.com>
2022-08-20 10:51:10 -0500

    fix phone not unique

--- apps/clients/models.py
+++ apps/clients/models.py
@@ -8,7 +8,7 @@ class Clients(models.Model):
     last_name = models.CharField(max_length=200)
     email = models.EmailField(unique=True)
     identification_number = models.CharField(max_length=200 ,blank=False, unique=True) 
-    phone = models.CharField(max_length=200 ,unique=True, blank=False)
+    phone = models.CharField(max_length=200, blank=False)
     second_phone = models.CharField(max_length=200 ,unique=True, blank=True,null=True)
     age = models.IntegerField(blank=True, null=True,default=None)
     birth_day = models.IntegerField(blank=True, null=True,default=None)
